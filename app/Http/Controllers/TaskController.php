a4bb0323d3e97ca203362a0339f5767e98cf7e96
NazninNahar1 <naznin@schertech.com>
2023-07-16 14:58:28 +0600

    pagination added

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,15 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
+
     {
-        $tasks = Task::all();
+        $rules = [
+            'page' => 'integer|min:1',
+            'per_page' => 'integer|min:1|max:100',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        $page = $request->input('page', 1);
+        $perPage = $request->input('per_page', 5);
+
+        $tasks = Task::paginate($perPage);
+        $tasks = Task::paginate($perPage);
+
         return response()->json($tasks);
     }
 
